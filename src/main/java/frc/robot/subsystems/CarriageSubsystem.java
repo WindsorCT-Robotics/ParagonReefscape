56ad98dc50c4b500d838122bcd01847a23e7e725
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2025-01-30 21:01:49 -0500

    worked on carriage subsystem

---
+++ src/main/java/frc/robot/subsystems/CarriageSubsystem.java
@@ -0,0 +1,97 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Units.Percent;
+import frc.robot.Units.Rotations;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.sim.SparkMaxAlternateEncoderSim;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class CarriageSubsystem extends SubsystemBase {
+    // private static final int ROLLER_MOTOR_CANID = 15;
+    private static final int OUTTAKE_MOTOR_CANID = 15;
+    private static final int INTAKE_MOTOR_CANID = 16;
+    private static final int BEAM_BREAKER_PIN = 2;
+
+    private SparkMax rollers;
+    private SparkMax outtakeRollers;
+    private SparkMax intakeRollers;
+    private SparkMaxConfig rollersConfig;
+    private final DigitalInput beamBreaker;
+    private RelativeEncoder rollerEncoder;
+    private RelativeEncoder outtakeEncoder;
+    private RelativeEncoder intakeEncoder;
+
+    private static final int MOTOR_MAX_RPM = 11000;
+    private static final double MOTOR_GEAR_RATIO = (double) 1 / 16; // Gear ratio is 4:1, 4:1
+
+    private static final double ROLLERS_CIRCUMFERENCE = Math.PI * 1.67; // Outer diameter is 1.67"
+
+    public CarriageSubsystem() {
+        outtakeRollers = new SparkMax(OUTTAKE_MOTOR_CANID, MotorType.kBrushless);
+        intakeRollers = new SparkMax(INTAKE_MOTOR_CANID, MotorType.kBrushless);
+        rollersConfig = new SparkMaxConfig();
+
+        rollersConfig.inverted(true);
+        rollersConfig.idleMode(IdleMode.kBrake);
+
+        rollers.configure(rollersConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        beamBreaker = new DigitalInput(BEAM_BREAKER_PIN);
+        rollerEncoder = rollers.getEncoder();
+        outtakeEncoder = outtakeRollers.getEncoder();
+        intakeEncoder = outtakeRollers.getEncoder();
+
+        SmartDashboard.putNumber("Outtake Percent Speed", CarriageSubsystem.targetRPM.asDouble() / (MOTOR_MAX_RPM * MOTOR_GEAR_RATIO));
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("Outtake Beam Breaker ", beamBreaker.get());
+    }
+
+    public void moveRollers(Percent speed) {
+        rollers.set(speed.asDouble());
+    }
+
+    public void outtakeRollers() {
+        Percent speed = new Percent(CarriageSubsystem.targetRPM.asDouble() / (MOTOR_MAX_RPM * MOTOR_GEAR_RATIO));
+        // rollers.set(speed.asDouble());
+        outtakeRollers.set(speed.asDouble());
+    }
+
+    public void intakeRollers() {
+        Percent speed = new Percent(CarriageSubsystem.targetRPM.asDouble() / (MOTOR_MAX_RPM * MOTOR_GEAR_RATIO));
+        // rollers.set(speed.asDouble());
+        intakeRollers.set(speed.asDouble());
+    }
+
+    public boolean isBeamBroken() {
+        return !beamBreaker.get();
+    }
+
+    public double getRollerPosition() { // Stopped on this.
+        // return outtakeEncoder.getPosition() intakeEncoder.getPosition(); 
+        // rollerEncoder.getPosition();
+    }
+
+    public void resetRollerEncoder() {
+        // rollerEncoder.setPosition(0.0);
+        rollerEncoder.setPosition(0.0);
+    }
+    
+    public void stop() {
+    // rollers.stopMotor();
+    intakeRollers.stopMotor();
+    outtakeRollers.stopMotor();
+    }
+}
