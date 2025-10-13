329fb513137358bb73076b880fac2a990452ef83
Charles Joseph Christie II <charles.christie.ii@hey.com>
2025-10-13 00:37:35 -0400

    Begin vision subsystem rewrite
    The current vision subsystem is tightly coupled to Limelight and depends on other subsystems to perform its calculations. A new, hardware-agnostic singularly-focused vision perception subsystem will replace it.

---
+++ src/main/java/frc/robot/subsystems/limelight/VisionSubsystem.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems.limelight;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.Limelight.LimelightHelpers.PoseEstimate;
+import frc.lib.Limelight.RectanglePoseArea;
+import frc.robot.hardware.IPerceptionCamera;
+import frc.robot.result.Failure;
+import frc.robot.result.Result;
+import frc.robot.result.Success;
+import frc.robot.units.Degrees;
+import frc.robot.units.Meters;
+
+public class VisionSubsystem extends SubsystemBase {
+    public sealed interface VisionError permits NoTags, PoseOutOfBounds { }
+    public record NoTags() implements VisionError { }
+    public record PoseOutOfBounds(Pose2d invalidPose) implements VisionError { }
+    
+    private final IPerceptionCamera camera;
+    private static final Meters FIELD_LENGTH = new Meters(16.54);
+    private static final Meters FIELD_WIDTH = new Meters(8.02);
+    private static final RectanglePoseArea field = new RectanglePoseArea(
+        new Translation2d(0.0, 0.0)
+        , new Translation2d(
+            FIELD_LENGTH.asDouble()
+            , FIELD_WIDTH.asDouble()));
+    
+    public VisionSubsystem(String subsystemName, IPerceptionCamera camera) {
+        super(subsystemName);
+        
+        this.camera = camera;
+        CameraServer.addCamera(camera.getCamera());
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        super.initSendable(builder);
+
+        
+    }
+
+    public Result<Pose2d, VisionError> estimatePosition(Degrees yaw) {
+        PoseEstimate result;
+
+        camera.setOrientation(yaw);
+        result = camera.estimateRobotPosition();
+
+        if (result.tagCount < 1) {
+            return new Failure<>(new NoTags());
+        }
+
+        if (!field.isPoseWithinArea(result.pose)) {
+            return new Failure<>(new PoseOutOfBounds(result.pose));
+        }
+
+        return new Success<>(result.pose);
+    }
+}
