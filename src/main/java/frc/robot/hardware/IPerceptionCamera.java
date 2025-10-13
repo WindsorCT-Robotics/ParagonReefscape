2fef7be0d6c269800506c58e79555421396ff86e
Charles Joseph Christie II <charles.christie.ii@hey.com>
2025-10-13 00:40:47 -0400

    Add Yaw parameter to estimateRobotPosition
    It seems that all vision subsystems require a valid yaw value to estimate the robot position.

--- src/main/java/frc/robot/hardware/IPerceptionCamera.java
+++ src/main/java/frc/robot/hardware/IPerceptionCamera.java
@@ -3,16 +3,16 @@ package frc.robot.hardware;
 import java.util.Optional;
 
 import frc.lib.Limelight.LimelightHelpers.PoseEstimate;
+import frc.robot.units.Degrees;
 import frc.robot.units.Meters;
 
 public interface IPerceptionCamera extends ICamera {
 
-    PoseEstimate estimateRobotPosition();
+    PoseEstimate estimateRobotPosition(Degrees yaw);
 
     boolean isEnabled();
 
     boolean hasDetectedValidTarget();
 
     Optional<Meters> distanceToTarget();
-
 }
