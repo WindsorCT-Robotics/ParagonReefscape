e50f369029eaea671f17252e8d6716a03b67d195
RobiticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2025-10-13 20:32:49 -0400

    Fix compilation error in VisionSubsystem

--- src/main/java/frc/robot/subsystems/limelight/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/limelight/VisionSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.limelight;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.Limelight.LimelightHelpers.PoseEstimate;
 import frc.lib.Limelight.RectanglePoseArea;
@@ -17,7 +16,7 @@ import frc.robot.units.Meters;
 public class VisionSubsystem extends SubsystemBase {
     public sealed interface VisionError permits NoTags, PoseOutOfBounds { }
     public record NoTags() implements VisionError { }
-    public record PoseOutOfBounds(Pose2d invalidPose) implements VisionError { }
+    public record PoseOutOfBounds(PoseEstimate invalidPose) implements VisionError { }
     
     private final IPerceptionCamera camera;
     private static final Meters FIELD_LENGTH = new Meters(16.54);
@@ -35,25 +34,17 @@ public class VisionSubsystem extends SubsystemBase {
         CameraServer.addCamera(camera.getCamera());
     }
 
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        super.initSendable(builder);
-
-        
-    }
-
     public Result<Pose2d, VisionError> estimatePosition(Degrees yaw) {
         PoseEstimate result;
 
-        camera.setOrientation(yaw);
-        result = camera.estimateRobotPosition();
+        result = camera.estimateRobotPosition(yaw);
 
         if (result.tagCount < 1) {
             return new Failure<>(new NoTags());
         }
 
         if (!field.isPoseWithinArea(result.pose)) {
-            return new Failure<>(new PoseOutOfBounds(result.pose));
+            return new Failure<>(new PoseOutOfBounds(result));
         }
 
         return new Success<>(result.pose);
