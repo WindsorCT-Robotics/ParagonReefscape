620bbba6cafbb3f59f7dd8c51b2f1bc20132e5ff
Charles Joseph Christie II <charles.christie.ii@hey.com>
2025-10-17 23:21:15 -0400

    Add empty commands and new unit of measure for swerve drive subsytem

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -1,9 +1,28 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.units.MetersPerSecond;
+import frc.robot.units.Radians;
+import frc.robot.units.RadiansPerSecond;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
     public SwerveDriveSubsystem() {
 
+    }
+    
+    public void robotOrientedDrive(MetersPerSecond longditudinalSpeed, MetersPerSecond lateralSpeed, RadiansPerSecond rotationalSpeed)
+    {
+
+    }
+
+    public void fieldOrientedDrive(MetersPerSecond allianceSpeed, MetersPerSecond wallSpeed, RadiansPerSecond rotationalSpeed, Radians currentAngle)
+    {
+
+    }
+
+    public void rotateAround(Translation2d centerOfRotation, RadiansPerSecond rotationSpeed)
+    {
+        
     }
 }
