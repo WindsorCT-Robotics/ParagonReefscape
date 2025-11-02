2fca86818f7f9d52bbc5be008316c118c5999066
Charles Joseph Christie II <charles.christie.ii@hey.com>
2025-11-01 21:23:56 -0400

    Update elevator motor simulation class to use new constructor

--- src/main/java/frc/robot/hardware/impl/sim/SimElevatorMotor.java
+++ src/main/java/frc/robot/hardware/impl/sim/SimElevatorMotor.java
@@ -4,20 +4,18 @@ import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.measure.Angle;
 import frc.robot.hardware.impl.ElevatorMotor;
 import frc.robot.hardware.sim.ISimMotor;
-import frc.robot.units.GearRatio;
 
-import org.dyn4j.geometry.Mass;
 import org.littletonrobotics.junction.Logger;
 
 
 public class SimElevatorMotor extends ElevatorMotor implements ISimMotor {
-    private final SparkMaxSim elevSim = new SparkMaxSim(elevMotor, DCMotor.getNEO(1));
+    private final SparkMaxSim elevSim;
 
-    public SimElevatorMotor(SparkMax motor, GearRatio gearRatio, Angle pulleyCircumference, Mass elevatorWeight) {
-        super(motor, gearRatio, pulleyCircumference, elevatorWeight);
+    public SimElevatorMotor(SparkMax motor) {
+        super(motor);
+        elevSim = new SparkMaxSim(elevMotor, DCMotor.getNEO(1));
     }
 
     @Override
