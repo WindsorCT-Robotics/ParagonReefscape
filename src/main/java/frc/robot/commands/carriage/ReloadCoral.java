437125260e9d83826316d722790eb65bbeccbacd
RobiticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2025-11-03 20:54:38 -0500

    Create ReloadCoralCommand with UnloadCoralCommand and LoadCoralCommand

--- src/main/java/frc/robot/commands/carriage/ReloadCoral.java
+++ src/main/java/frc/robot/commands/carriage/ReloadCoral.java
@@ -1,7 +1,18 @@
 package frc.robot.commands.carriage;
 
+import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.carriage.CarriageSubsystem;
 
 public class ReloadCoral extends SequentialCommandGroup {
-    
+    public ReloadCoral(CarriageSubsystem carriageSubsystem, Dimensionless speed) {
+        addCommands(
+                new UnloadCoral(carriageSubsystem, speed),
+                new LoadCoral(carriageSubsystem, speed)
+        );
+    }
+
+    public ReloadCoral(CarriageSubsystem carriageSubsystem) {
+        this(carriageSubsystem, carriageSubsystem.getDefaultSpeed());
+    }
 }
