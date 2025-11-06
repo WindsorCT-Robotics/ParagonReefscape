26c396e25cf077576aed55cb186f182257f8d50a
RobiticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2025-11-05 20:05:52 -0500

    Create Drive Commands: Field Relative, Robot Relative, and Move Robot

---
+++ src/main/java/frc/robot/commands/drive/TranslateAbsoluteCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.drive;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class TranslateAbsoluteCommand extends Command {
+    private final CommandSwerveDrivetrain drivetrain;
+    private final LinearVelocity MaxVelocity;
+    private final AngularVelocity MaxAngularRate;
+    private FieldCentric centricDrive;
+    private CommandXboxController controller;
+
+    public TranslateAbsoluteCommand(CommandSwerveDrivetrain drivetrain, CommandXboxController controller) {
+        this.drivetrain = drivetrain;
+        this.MaxVelocity = drivetrain.getMaxVelocity();
+        this.MaxAngularRate = drivetrain.getMaxAngularRate();
+        this.controller = controller;
+    }
+
+    @Override
+    public void execute() {
+        new DriveCommand(drivetrain, () -> centricDrive(controller));
+    }
+
+    private FieldCentric centricDrive(CommandXboxController controller) {
+        return centricDrive
+            .withVelocityX(-controller.getLeftY() * Math.abs(controller.getLeftY()) * MaxVelocity.in(MetersPerSecond)) // Drive forward with negative Y (forward)
+            .withVelocityY(-controller.getLeftX() * Math.abs(controller.getLeftX()) * MaxVelocity.in(MetersPerSecond)) // Drive left with negative X (left)
+            .withRotationalRate(-controller.getRightX() * Math.abs(controller.getRightX()) * MaxAngularRate.in(RadiansPerSecond));
+    }
+}
---
+++ src/main/java/frc/robot/commands/drive/TranslateRelativeCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.drive;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveRequest.RobotCentric;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class TranslateRelativeCommand extends Command {
+    private final CommandSwerveDrivetrain drivetrain;
+    private final LinearVelocity MaxVelocity;
+    private final AngularVelocity MaxAngularRate;
+    private RobotCentric robotCentric;
+    private CommandXboxController controller;
+
+    public TranslateRelativeCommand(CommandSwerveDrivetrain drivetrain, CommandXboxController controller) {
+        this.drivetrain = drivetrain;
+        this.MaxVelocity = drivetrain.getMaxVelocity();
+        this.MaxAngularRate = drivetrain.getMaxAngularRate();
+        this.controller = controller;
+    }
+
+    @Override
+    public void execute() {
+        new DriveCommand(drivetrain, () -> centricDrive(controller));
+    }
+
+    private RobotCentric centricDrive(CommandXboxController controller) {
+        return robotCentric
+            .withVelocityX(-controller.getLeftY() * Math.abs(controller.getLeftY()) * MaxVelocity.in(MetersPerSecond)) // Drive forward with negative Y (forward)
+            .withVelocityY(-controller.getLeftX() * Math.abs(controller.getLeftX()) * MaxVelocity.in(MetersPerSecond)) // Drive left with negative X (left)
+            .withRotationalRate(-controller.getRightX() * Math.abs(controller.getRightX()) * MaxAngularRate.in(RadiansPerSecond));
+    }
+}
