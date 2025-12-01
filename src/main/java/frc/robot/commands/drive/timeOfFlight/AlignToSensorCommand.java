892c9a0d0e40393cd7d2334f0d898f2c2f9372d4
SuppressedCoder <aldwinpham@gmail.com>
2025-12-01 18:54:51 -0500

    Implement methods to move CommandSwerveDrivetrain with AlignToSensorCommand

--- src/main/java/frc/robot/commands/drive/timeOfFlight/AlignToSensorCommand.java
+++ src/main/java/frc/robot/commands/drive/timeOfFlight/AlignToSensorCommand.java
@@ -1,40 +1,45 @@
 package frc.robot.commands.drive.timeOfFlight;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Millimeters;
+
+import com.ctre.phoenix6.swerve.SwerveRequest.RobotCentric;
 
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.TimeOfFlight.TimeOfFlightSubsystem;
 
-public class AlignToSensor extends Command {
+public class AlignToSensorCommand extends Command {
     private final CommandSwerveDrivetrain drivetrain;
     private final TimeOfFlightSubsystem timeOfFlight;
     private final LinearVelocity ALIGN_VELOCITY_Y;
     private final HorizontalDirection direction;
+    private final RobotCentric driveRequest;
 
-    public AlignToSensor(CommandSwerveDrivetrain drivetrain, TimeOfFlightSubsystem timeOfFlight, HorizontalDirection direction) {
+    public AlignToSensorCommand(CommandSwerveDrivetrain drivetrain, TimeOfFlightSubsystem timeOfFlight, HorizontalDirection direction) {
         this.drivetrain = drivetrain;
         this.timeOfFlight = timeOfFlight;
         this.direction = direction;
         this.ALIGN_VELOCITY_Y = drivetrain.getDefaultTOFSpeed();
+        driveRequest = new RobotCentric();
     }
 
     @Override
     public void initialize() {
         switch (direction) {
             case LEFT:
-                // Moving Left Command
+                moveLeft();
                 break;
             case RIGHT:
-                // Moving Right Command
+                moveRight();
                 break;
         }
     }
 
     @Override
     public void end(boolean interrupted) {
-        // Stop the drivetrain when the command ends
+        stop();
     }
 
     @Override
@@ -51,5 +56,20 @@ public class AlignToSensor extends Command {
             default:
                 return true;
         }
+    }
+
+    private void moveLeft() {
+        drivetrain.setControl(driveRequest.withVelocityY(ALIGN_VELOCITY_Y));
+    }
+
+    private void moveRight() {
+        drivetrain.setControl(driveRequest.withVelocityY(ALIGN_VELOCITY_Y.times(-1)));
+    }
+
+    private void stop() { // TODO: Should this be a part of CommandSwerveDrivetrain?
+        drivetrain.setControl(
+            driveRequest
+            .withVelocityX(MetersPerSecond.of(0))
+            .withVelocityY(MetersPerSecond.of(0)));
     }
 }
