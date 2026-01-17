ef12190917cab2320e780f67393c770db773aa83
SuppressedCoder <aldwinpham@gmail.com>
2026-01-17 18:06:57 -0500

    Rename limelight package to vision

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,7 +1,9 @@
-package frc.robot.subsystems.limelight;
+package frc.robot.subsystems.vision;
 
 import static edu.wpi.first.units.Units.Meter;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
