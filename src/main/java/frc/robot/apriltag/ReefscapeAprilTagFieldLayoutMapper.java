9dcc8456e7ad98e59e4ca1479debdb5d7ec7996f
SuppressedCoder <aldwinpham@gmail.com>
2026-01-27 17:32:08 -0500

    Change case format for switch to fix sonarqube issue

--- src/main/java/frc/robot/apriltag/ReefscapeAprilTagFieldLayoutMapper.java
+++ src/main/java/frc/robot/apriltag/ReefscapeAprilTagFieldLayoutMapper.java
@@ -28,30 +28,13 @@ public final class ReefscapeAprilTagFieldLayoutMapper {
         AprilTagLocation location;
 
         switch (tag.ID) {
-            case 1:
-            case 2:
-            case 12:
-            case 13:
+            case 1, 2, 12, 13:
                 location = AprilTagLocation.SOURCE;
                 break;
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 17:
-            case 18:
-            case 19:
-            case 20:
-            case 21:
-            case 22:
+            case 6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22:
                 location = AprilTagLocation.REEF;
                 break;
-            case 4:
-            case 5:
-            case 14:
-            case 15:
+            case 4, 5, 14, 15:
                 location = AprilTagLocation.BARGE;
                 break;
             default:
